7bd49547dd71a5371fe36e3205b39736a6981925
TAI-PC\TAI <teoteoteo99@ymail.com>
2018-11-28 00:10:22 +0700

    - Create Road

---
+++ CubeRun/Assets/Scripts/Manager/RoadManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadManager : MonoBehaviour {
+
+    public GameObject road;
+    public int length;
+    private Vector3 posBegin;
+    
+	void Start () {
+        CreateRoads();
+	}
+	void Update () {
+		
+	}
+    void CreateRoads()
+    {
+        // Vị trí xuất hiện của road đầu tiên là chính tọa độ của RoadManager
+        posBegin = transform.position;
+        for (int i = 0; i < length; ++i)
+        {
+            GameObject obj;
+            // Xuất đối tượng nào (GameObject), tại vị trí nào (Vector3), không quay(Quaternion.identity)
+            obj = Instantiate(road, posBegin, Quaternion.identity);
+            // Lệnh cho obj tạo Obstancle
+            obj.GetComponent<RoadController>().CreateObstancle();
+            // gán đối tượng vừa sinh ra làm con của RoadManager
+            obj.transform.parent = transform;
+            // gán tên cho Road mới sinh
+            obj.name = "Road " + i;
+            // cập nhật cho vị trí tiếp theo
+            // mõi road cách nhau 2 đơn vị
+            posBegin.z += transform.localScale.z;
+        }
+    }
+}
---
+++ CubeRun/Assets/Scripts/Roads/RoadController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadController : MonoBehaviour {
+
+    public GameObject[] listObatancle;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void CreateObstancle()
+    {
+        // Vị trí xuất hiện vật cản
+        Vector3 posCreate = transform.position;
+        // Thứ tự của Obstancle trong  listObstancle
+        // Lấy ngẫu nhiên từ thứ tự 0 -> thứ tự cuối cùng là listObatancle.Length
+        int index = Random.Range(0, listObatancle.Length);
+        // Xuất đối tượng tại thứ tự index ra vị trí posCreate.
+        GameObject obj;
+        obj = Instantiate(listObatancle[index], posCreate, Quaternion.identity);
+        // gán đối tượng vừa tạo, là con của Road hiện tại.
+        obj.transform.parent = transform;
+    }
+}
